6139b4348422d7b572fb3c7a9d2e6eaedd838123
naveenbish <bishtnitin003@gmail.com>
2024-05-25 15:23:11 +0530

    react http API testing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar from './components/Navbar';
 import FootBar from './components/FootBar';
 import BodyStuff from './components/BodyStuff';
+import ApiTest from './components/ApiTest';
 
 function App() {
   return (
@@ -11,6 +12,10 @@ function App() {
       
       {/* main bar component  */}
         <BodyStuff></BodyStuff>
+
+        <div>
+          <ApiTest />
+        </div>
 
       {/* Footbar component  */}
         <div className='bg-[#0e101b] text-white w-full h-[240px] flex justify-center items-center'>
---
+++ src/components/ApiTest.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const ApiTest = () => {
+  const [logoData, setLogoData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://13.201.55.203:3000/add-logo');
+        setLogoData(response.data);
+        console.log(logoData)
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h2>Logo Data</h2>
+        <div>
+            <p>{logoData[0]._id}</p>
+        </div>
+    </div>
+  );
+};
+
+export default ApiTest;
